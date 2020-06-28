059631fe52fce706fdfbcaee05d5ba71e827aa82
55 SH4D0W <fac929@gmail.com>
2020-06-28 16:01:24 +0400

    Upload

---
+++ StudentClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClass
+{
+    class Undergard
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetUndergradInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Группа: {place}|");
+        }
+    }
+
+    
+    class Classman : Undergard
+    {
+        public string type { get; set; }
+
+        public virtual void GetClassmanInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Возраст: {age}| |Группа: {place}| |Стоимость обучения (в руб): {price}| |Тип: {type}");
+        }
+    }
+}
---
+++ StudentClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Undergard undergrad = new Undergard();
+            undergrad.name = "Тест Тестович";
+            undergrad.price = 45;
+            undergrad.age = 19;
+            undergrad.place = "ПКС-18-01";
+            
+            undergrad.GetUndergradInfo();
+
+            Classman classman = new Classman();
+            classman.type = "Платник";
+            classman.name = "Иванов.И.И";
+            classman.place = "ССА-20-01";
+            classman.price = 35000;
+            classman.age = 20;
+
+            classman.GetClassmanInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
